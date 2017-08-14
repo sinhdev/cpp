bac1c6aacc8126e292ea9c156fd1c6eac018bed8
Nguyễn Xuân Sinh <sinhnx.dev@gmail.com>
2017-08-14 19:41:41 +0700

    Multithreading demo

---
+++ Multithreading/FunctionPointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+
+void thread_function(){
+    for(int i = 0; i < 10000; i++)
+        cout<<"thread function Executing"<<endl;
+}
+int main(int count, const char* args[]){
+    thread threadObj(thread_function);
+    for(int i = 0; i < 10000; i++)
+        cout<<"Display From MainThread"<<endl;
+    threadObj.join();    
+    cout<<"Exit of Main function"<<endl;
+    return 0;
+}
